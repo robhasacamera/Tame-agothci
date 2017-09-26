ac6e967f7130be3f0b3f4b976736337ac2c5f0c5
Robert Cole <robhasacamera@gmail.com>
2017-09-26 12:11:23 -0500

    Initial Commit

---
+++ Tame-agothci/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tame-agothci
+//
+//  Created by Robert J Cole on 9/26/17.
+//  Copyright © 2017 Robert Cole. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

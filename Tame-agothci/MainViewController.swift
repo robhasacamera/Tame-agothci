e71e2e171ec446b99919e5475bb7958372bf3a94
Dan Wilson <dan@drwdesigns.com>
2017-10-01 08:55:22 -0500

    10.01.17: Added timer functions for Pet, Comb and Play buttons.

--- Tame-agothci/MainViewController.swift
+++ Tame-agothci/MainViewController.swift
@@ -14,11 +14,17 @@ class MainViewController: UIViewController {
     
     var timer: Timer?
     
-    var lastTimeFed: Date?
+    var lastTimeFed:    Date?
+    var lastTimePet:    Date?
+    var lastTimeCombed: Date?
+    var lastTimePlayed: Date?
     
     let feedInterval = 10.0
+    let petInterval  = 10.0
+    let combInterval = 20.0
+    let playInterval = 45.0
     
-    var lion = Lion(hunger: 70, happiness: 10)
+    var lion = Lion(hunger: 30, happiness: 10)
     
     
     override func viewDidLoad() {
@@ -38,7 +44,7 @@ class MainViewController: UIViewController {
     @IBAction func feedButtonPressed(_ sender: Any) {
         print("Feed was pressed")
         feedButton.isEnabled = false
-        lion.hunger = lion.hunger - 10
+        lion.hunger -= 10
         lastTimeFed = Date()
     }
     
@@ -46,18 +52,27 @@ class MainViewController: UIViewController {
     
     @IBAction func petButtonPressed(_ sender: Any) {
         print("Pet was pressed")
+        petButton.isEnabled = false
+        lion.happiness += 5
+        lastTimePet = Date()
     }
     
     @IBOutlet weak var combButton: UIButton!
     
     @IBAction func combButtonPressed(_ sender: Any) {
         print("Comb was pressed")
+        combButton.isEnabled = false
+        lion.happiness += 10
+        lastTimeCombed = Date()
     }
     
     @IBOutlet weak var playButton: UIButton!
     
     @IBAction func playButtonPressed(_ sender: Any) {
         print("Play was pressed")
+        playButton.isEnabled = false
+        lion.happiness += 15
+        lastTimePlayed = Date()
     }
     
     @IBOutlet weak var trainButton: UIButton!
@@ -79,6 +94,33 @@ class MainViewController: UIViewController {
             let timeSinceLastFed = -1 * lastTimeFed.timeIntervalSinceNow
             if timeSinceLastFed >= feedInterval {
                 feedButton.isEnabled = true
+            }
+        }
+    }
+    
+    func updatePetButton() {
+        if let lastTimePet = lastTimePet {
+            let timeSinceLastPet = -1 * lastTimePet.timeIntervalSinceNow
+            if timeSinceLastPet >= petInterval {
+                petButton.isEnabled = true
+            }
+        }
+    }
+    
+    func updateCombButton() {
+        if let lastTimeCombed  = lastTimeCombed {
+            let timeSinceLastCombed = -1 * lastTimeCombed.timeIntervalSinceNow
+            if timeSinceLastCombed >= combInterval {
+                combButton.isEnabled = true
+            }
+        }
+    }
+    
+    func updatePlayButton() {
+        if let lastTimePlayed = lastTimePlayed {
+            let timeSinceLastPlayed = -1 * lastTimePlayed.timeIntervalSinceNow
+            if timeSinceLastPlayed >= playInterval {
+                playButton.isEnabled = true
             }
         }
     }

c47762d9403b01ce501e777540c1a338fbd356c3
Dan Wilson <dan@drwdesigns.com>
2017-10-19 11:33:35 -0500

    Limited ranges for hunger and happiness

--- Tame-agothci/Lion.swift
+++ Tame-agothci/Lion.swift
@@ -9,8 +9,24 @@
 import Foundation
 
 public class Lion {
-    var hunger: Int
-    var happiness: Int
+    var hunger: Int {
+        didSet {
+            if hunger > 100 {
+                hunger = 100
+            } else if hunger < 0 {
+                hunger = 0
+            }
+        }
+    }
+    var happiness: Int {
+        didSet {
+            if happiness < 0 {
+                happiness = 0
+            } else if happiness > 100 {
+                happiness = 100
+            }
+        }
+    }
     var level: Int
     public init (hunger: Int, happiness: Int){
         level = 1
